666b85462401ae292dec15bc6ab958791304f6ea
bhushan <bhushanmandala456@gmail.com>
2024-08-02 15:09:26 +0530

    all set for praveen and bhanu

--- backend/models/Accomodation.js
+++ backend/models/Accomodation.js
@@ -1,12 +1,35 @@
 const mongoose = require('mongoose');
 
 const AccommodationSchema = new mongoose.Schema({
-  type: String, // e.g., Deluxe Tent, Shared Room
-  description: String,
-  price: Number,
-  shared: Boolean,
-  amenities: [String],
-  images: [String] // Array of image URLs
+  type: { 
+    type: String, 
+    required: [true, 'Accommodation type is required'] 
+  },
+  description: { 
+    type: String, 
+    required: [true, 'Accommodation description is required'] 
+  },
+  price: { 
+    type: Number, 
+    required: [true, 'Accommodation price is required'] 
+  },
+  shared: { 
+    type: Boolean, 
+    required: [true, 'Shared status is required'] 
+  },
+  amenities: { 
+    type: [String], 
+    required: [true, 'Amenities are required'] 
+  },
+  images: { 
+    type: [String], 
+    required: [true, 'Accommodation images are required'] 
+  },
+  retreatcenter: { 
+    type: mongoose.Schema.Types.ObjectId, // Reference to the Retreat model
+    ref: 'Retreat', // The name of the model being referenced
+    required: [true, 'Retreat reference is required'] 
+  }
 });
 
 module.exports = mongoose.model('Accommodation', AccommodationSchema);
---
+++ backend/models/bookingmodel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const BookingSchema = new mongoose.Schema({
+  user: { 
+    type: mongoose.Schema.Types.ObjectId, // Reference to the User model
+    ref: 'User', // The name of the model being referenced
+    required: [true, 'User reference is required'] 
+  },
+  accommodation: { 
+    type: mongoose.Schema.Types.ObjectId, // Reference to the Accommodation model
+    ref: 'Accommodation', // The name of the model being referenced
+    required: [true, 'Accommodation reference is required'] 
+  },
+  retreatcenter: { 
+    type: mongoose.Schema.Types.ObjectId, // Reference to the Retreat model
+    ref: 'Retreat', // The name of the model being referenced
+    required: [true, 'Retreat reference is required'] 
+  },
+  checkInDate: { 
+    type: Date, 
+    required: [true, 'Check-in date is required'] 
+  },
+  checkOutDate: { 
+    type: Date, 
+    required: [true, 'Check-out date is required'] 
+  },
+  numberOfGuests: { 
+    type: Number, 
+    required: [true, 'Number of guests is required'] 
+  },
+//   specialRequests: { 
+//     type: String // Optional field for special requests
+//   },
+  status: { 
+    type: String, 
+    enum: ['Pending', 'Confirmed', 'Cancelled'], 
+    default: 'Pending' 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+  updatedAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+module.exports = mongoose.model('Booking', BookingSchema);
