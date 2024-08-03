670084b9c051db8a27e9de45b5c391d8ac0baf4f
jeerupraveen04 <praveenjeeru68@gmail.com>
2024-08-03 22:18:47 +0530

    hii

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -43,7 +43,7 @@ export default function Homepage() {
       { retreatdata.map((data,index)=>{
         return <RetreatDiv da={data} />
       })
-        }
+      }
       <Footer/>
     </div>
   );
