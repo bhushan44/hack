ecbf9457b7b6e06f222ef8b655ba3ef2cddcf958
jeerupraveen04 <praveenjeeru68@gmail.com>
2024-08-03 21:14:55 +0530

    yes

---
+++ frontend/src/Components/RetreatDiv.js
@@ -0,0 +1,23 @@
+
+function RetreatDiv({da}) {
+    console.log(da)
+    return (
+        <div className="flex flex-col items-center border border-gray-300 rounded-lg overflow-hidden shadow-md w-400px m-5 p-5 bg-white text-center">
+            <div className="w-full max-w-[150px] border-b border-gray-300">
+                <img src={da.images[0]} alt="Center" className="w-full h-auto" />
+            </div>
+            <div className="my-5">
+                <h3 className="text-lg font-semibold">{da.name}</h3>
+                <h4 className="text-lg font-semibold">{da.location}</h4>
+                <p className="text-gray-700">{da.description}</p>
+            </div>
+            <div className="mt-2">
+                <button className="px-5 py-2 border-none rounded bg-blue-500 text-white cursor-pointer transition-colors duration-300 hover:bg-blue-700">
+                    Show More
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default RetreatDiv;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -6,7 +6,7 @@ import { IoFlagOutline } from "react-icons/io5";
 import { MdOutlineGroup } from "react-icons/md";
 import { useEffect, useState } from "react";
 import Header from "../Components/Header";
-import Retreatcen from "../Components/Retreatcen";
+import RetreatDiv from "../Components/RetreatDiv";
 import Retreatscroll from "../Components/Retreatscroll";
 import "./Homepage.css";
 import { useNavigate } from "react-router-dom";
@@ -14,17 +14,36 @@ import Footer from "../Components/Footer";
 
 export default function Homepage() {
   const navigate=useNavigate()
+  const [retreatdata,setRetreatData]=useState([]);
   function details(id){
     navigate(`/overview/${id}`)
 
   }
   const [data, setData] = useState([]);
- 
+  const retData=async()=>{
+    try{
+      const refetch= await fetch('http://localhost:5000/api/v1/getretreats',{
+        method:"GET",
+        headers:{"content-type":"application/json"}
+      })
+      const redata=await refetch.json();
+      setRetreatData(redata)
+    }catch(e){
+      console.error(e)
+    }
+  }
+  useEffect(() => {
+    retData();
+  }, []);
+  console.log(retreatdata);
   return (
     <div className="bg-slate-200">
       <Header></Header>
       <Retreatscroll/>
-      <Retreatcen />
+      { retreatdata.map((data,index)=>{
+        return <RetreatDiv da={data} />
+      })
+        }
       <Footer/>
     </div>
   );
