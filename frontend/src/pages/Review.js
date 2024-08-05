9a772e076629cf252c2d551c5026070ad2ada33a
gamyageethika <gamyageethika330@gmail.com>
2024-08-05 19:19:57 +0530

    hii

--- frontend/src/pages/Review.js
+++ frontend/src/pages/Review.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Review() {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
   return (
     <>
-    <div className='min-h-44 text-3xl'>
-        <h1>Feedback</h1>
-        
-    </div>
-    <div className='text-3xl'>
+      <div className="text-3xl">
         <h1>Reviews</h1>
-    </div>
+        <div className="flex-container">
+          {[1, 2, 3, 4, 5].map(function(star) {
+            return (
+              <span
+                key={star}
+                className={star <= rating ? 'fa fa-star checked' : 'fa fa-star'}
+                onClick={function() { handleRating(star); }}
+                style={{
+                  cursor: 'pointer',
+                  marginRight: '5px',
+                  color: star <= rating ? 'orange' : 'gray'
+                }}
+              ></span>
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
